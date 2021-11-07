a374106960a70f1ad0debebef26e21dea1dc2a38
apaspartu <48532460+apaspartu@users.noreply.github.com>
2021-11-07 20:56:59 +0200

    Changed javascript, added animations.

--- main.js
+++ main.js
@@ -1,22 +1,48 @@
-var id = null;
+quotes = ["Якщо космос має безмежний запас часу, це не просто означає, що може статися все, що завгодно. Це означає, що все коли-небудь дійсно станеться.",
+          "Астрономія змушує душу дивитися вгору і веде нас із цього світу до іншого.",
+          "Людство не залишиться вічно на Землі, але в гонитві за світлом і простором спочатку несміливо проникне за межі атмосфери, а потім завоює собі весь навколосонячний простір.",
+          "Космонавтика має безмежне майбутнє, і її перспективи неосяжні, як сам Всесвіт.",
+          "Наука ніколи не була і не буде закінченою книгою. Кожен важливий успіх ставить нові питання. Будь-який розвиток виявляє з часом усе нові й глибші труднощі.",
+          "Космос - це все, що є, було колись, і коли-небудь буде.",
+          "Найбільш незрозуміле у нашому світі є те, що він усе-таки зрозумілий."];
 
-function myf()
+authors = ["", "Платон", "Констянтин Ціолковський", "Сергій Корольов", "Альберт Ейнштейн", "Карл Саган", "Альберт Ейнштейн"];
+
+let quote_number = Math.floor(Math.random() * 7);
+
+function change_quote()
 {
-    var elem = document.getElementById("bigearth");
-  
-    var w = 46;
-    clearInterval(id);
-    id = setInterval(frame, 70);
+    document.querySelector("#quote-author").innerHTML = "";
 
-    function frame()
+    quote_number += 1;
+    if (quote_number == 7)
     {
-        if (w == 40) {
-        w = 46;  
+        quote_number = 0;
+    }
+
+    let author_prefix = "";
+    if (authors[quote_number] != "")
+    {
+        author_prefix = "- ";
+    }
+
+    let chars = 0;
+    let timer = setInterval(function() {
+        if (chars > quotes[quote_number].length + authors[quote_number].length)
+        {
+            clearInterval(timer);
+            return;
+        }
+        chars += 1;
+        if (chars <= quotes[quote_number].length)
+        {
+            document.querySelector("#quote-text").innerHTML = quotes[quote_number].slice(0, chars);
         }
         else
-        {
-            w -= 1;
-            elem.style.width = `${w}%`;
-        }
-    }
+        {        
+            document.querySelector("#quote-author").innerHTML = author_prefix + authors[quote_number].slice(0, chars - quotes[quote_number].length - 1);
+        } 
+    }, 20);
 }
+
+window.onload = change_quote;
